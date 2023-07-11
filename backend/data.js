a60325985d36b584b32efb857a777bb2b7cd8ea9
TalhaRahman <tallookhan1@gmail.com>
2023-07-11 09:09:29 +0500

    created a server

---
+++ backend/data.js
@@ -0,0 +1,38 @@
+const data = {
+  products: [
+    {
+      name: "pizza",
+      slug: "pizza",
+      price: 120,
+      description: "good Pizza",
+      image: "/images/p1.png",
+      countInStock: 50,
+      category: "Pizza",
+      rating: 4.3,
+      numReviews: 89,
+    },
+    {
+      name: "Burger",
+      slug: "Burger",
+      price: 120,
+      description: "good burder",
+      image: "/images/p2.png",
+      countInStock: 50,
+      category: "Burger",
+      rating: 4.3,
+      numReviews: 89,
+    },
+    {
+      name: "Shawarma",
+      slug: "Shawarma",
+      price: 120,
+      description: "good Shawarma",
+      image: "/images/p3.png",
+      countInStock: 50,
+      category: "shawarma",
+      rating: 4.3,
+      numReviews: 89,
+    },
+  ],
+};
+export default data;
