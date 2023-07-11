928496fe56067582c30c94e2cded5bd35b894d69
TalhaRahman <tallookhan1@gmail.com>
2023-07-11 08:42:02 +0500

    product shown

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,17 @@
 import "./App.css";
-
+import data from "./data";
 function App() {
   return (
     <div className='App'>
-      <h1>Hello</h1>
+      <h1>products</h1>
+      {data.products.map((product) => (
+        <div>
+          <img src={product.image} alt={product.image}></img>
+        </div>
+      ))}
     </div>
   );
 }
+// TODO react-routing and import data from laptop
 
 export default App;
