d791fb73b72a73ef3322a24449816d49b860c4f1
TalhaRahman <64687461+talhaRahman09@users.noreply.github.com>
2023-07-18 11:09:49 +0500

    cards component

--- frontend/src/pages/Home/index.jsx
+++ frontend/src/pages/Home/index.jsx
@@ -3,6 +3,7 @@
 import { useEffect, useReducer } from 'react';
 import axios from 'axios';
 import Header from '../../components/Header'
+import Cards from '../../components/Cards';
 
 
 const reducer = (state, action) => {
@@ -58,20 +59,37 @@ const Home = () => {
     <div>
       <Header />
 
-      <div>
-        {loading ? (
-          <p>Loading...</p>
-        ) : error ? (
-          <p>Error: {error}</p>
-        ) : products.length > 0 ? (
-          products.map((product) => (
-            <div key={product.slug}>
-              <img src={product.image} alt={product.slug} />
-            </div>
-          ))
-        ) : (
-          <p>No products available</p>
-        )}
+      <div className="container">
+        <div className='row'>
+          {loading ? (
+            <p>Loading...</p>
+          ) : error ? (
+            <p>Error: {error}</p>
+          ) : products.length > 0 ? (
+            products.map((product) => (
+
+              <div className="col-md-4">
+                <div className='cardsWrapper'>
+                  <div class="card">
+                    <div className="imgWrapper">
+                      <img src={product.image} class="card-img-top" alt="product-image" />
+                    </div>
+                    <div class="card-body p-2">
+                      <div className='d-flex justify-content-between align-items-center'>
+                        <h5 class="cardTitle">{product.name}</h5>
+                        <h5 className='cardPrice text-uppercase fw-bold'>pkr {product.price}</h5>
+                      </div>
+                      <p class="cardText">{product.description}</p>
+                      <button type='buttn' class="btn btnAddToBucket fw-bold">Add To Bucket</button>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            ))
+          ) : (
+            <p>No products available</p>
+          )}
+        </div>
       </div>
 
     </div>
