9341ef1c0602b746b28f720d759ee31ea65acff0
TalhaRahman <tallookhan1@gmail.com>
2023-08-03 15:33:19 +0500

    asdf

--- frontend/src/components/Header/index.jsx
+++ frontend/src/components/Header/index.jsx
@@ -51,7 +51,7 @@ const Header = () => {
                 <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="#E5BE69" className="bi bi-bucket" viewBox="0 0 16 16">
                   <path d="M2.522 5H2a.5.5 0 0 0-.494.574l1.372 9.149A1.5 1.5 0 0 0 4.36 16h7.278a1.5 1.5 0 0 0 1.483-1.277l1.373-9.149A.5.5 0 0 0 14 5h-.522A5.5 5.5 0 0 0 2.522 5zm1.005 0a4.5 4.5 0 0 1 8.945 0H3.527zm9.892 1-1.286 8.574a.5.5 0 0 1-.494.426H4.36a.5.5 0 0 1-.494-.426L2.58 6h10.838z" />
                   {cart.cartItems.length > 0 && (
-                    <text x="50%" y="64%" dy=".3em" textAnchor="middle" fontSize="6" fontWeight="bold">{cart.cartItems.length}</text>
+                    <text x="50%" y="64%" dy=".3em" textAnchor="middle" fontSize="6" fontWeight="bold">{cart.cartItems.reduce((a, c) => a + c.quantity, 0)}</text>
                   )}
                 </svg>
               </span>
