5153722297a7939398adfac83df5fb9203619a61
TalhaRahman <tallookhan1@gmail.com>
2023-08-08 22:28:44 +0500

    asd

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,26 +1,8 @@
 import './App.css';
-import Home from './pages/Home';
-import ProductScreen from './pages/ProductScreen'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import Header from './components/Header';
-import { Helmet } from 'react-helmet-async';
+
 
 function App() {
   return (
-    <BrowserRouter>
-      <Header />
-      <div className='container'>
-        <Helmet>
-          <title>Hafiz Pizza Hut</title>
-        </Helmet>
-        <div>
-          <Routes>
-            <Route path='/product/:slug' element={<ProductScreen />} />
-            <Route path='/' element={<Home />} />
-          </Routes>
-        </div>
-      </div>
-    </BrowserRouter>
 
   );
 }
