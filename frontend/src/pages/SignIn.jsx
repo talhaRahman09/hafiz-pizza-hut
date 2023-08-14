d3181b1ad882280e51590493327b6c2836ac9142
TalhaRahman <tallookhan1@gmail.com>
2023-08-14 22:08:02 +0500

    asd

--- frontend/src/pages/SignIn.jsx
+++ frontend/src/pages/SignIn.jsx
@@ -38,62 +38,61 @@ const SignIn = () => {
     };
 
     return (
-        <Layout>
 
-            <div className="container mt-5">
-                <div className="row justify-content-center">
-                    <div className="col-md-8">
-                        <img src="" alt="" />
-                    </div>
-                    <div className="col-md-4">
-                        <div className="card p-4">
+        <div className="container mt-5">
+            <div className="row justify-content-center">
+                <div className="col-md-8">
+                    <img src="" alt="" />
+                </div>
+                <div className="col-md-4">
+                    <div className="card p-4">
 
-                            <div className="card-body p-2">
-                                <div className="text-center">
-                                    <img className='logo' src={logo} alt="" />
-                                    <h3>Sign In</h3>
-                                </div>
-                                <form onSubmit={handleSubmit}>
-
-                                    <div className="mb-3">
-                                        <label htmlFor="email" className="form-label">
-                                            Email
-                                        </label>
-                                        <input
-                                            type="email"
-                                            className="form-control"
-                                            id="email"
-                                            name="email"
-                                            value={email}
-                                            onChange={(e) => setEmail(e.target.value)}
-                                            required
-                                        />
-                                    </div>
-                                    <div className="mb-3">
-                                        <label htmlFor="password" className="form-label">
-                                            Password
-                                        </label>
-                                        <input
-                                            type="password"
-                                            className="form-control"
-                                            id="password"
-                                            name="password"
-                                            value={password}
-                                            onChange={(e) => setPassword(e.target.value)}
-                                            required
-                                        />
-                                    </div>
-
-                                    <button type="submit" className="btn submitBtn w-100">
-                                        Sign In
-                                    </button>
-                                </form>
+                        <div className="card-body p-2">
+                            <div className="text-center">
+                                <img className='logo' src={logo} alt="" />
+                                <h3>Sign In</h3>
                             </div>
+                            <form onSubmit={handleSubmit}>
+
+                                <div className="mb-3">
+                                    <label htmlFor="email" className="form-label">
+                                        Email
+                                    </label>
+                                    <input
+                                        type="email"
+                                        className="form-control"
+                                        id="email"
+                                        name="email"
+                                        value={email}
+                                        onChange={(e) => setEmail(e.target.value)}
+                                        required
+                                    />
+                                </div>
+                                <div className="mb-3">
+                                    <label htmlFor="password" className="form-label">
+                                        Password
+                                    </label>
+                                    <input
+                                        type="password"
+                                        className="form-control"
+                                        id="password"
+                                        name="password"
+                                        value={password}
+                                        onChange={(e) => setPassword(e.target.value)}
+                                        required
+                                    />
+                                </div>
+
+                                <button type="submit" className="btn submitBtn w-100">
+                                    Sign In
+                                </button>
+                            </form>
                         </div>
                     </div>
                 </div>
             </div>
-        </Layout >
+        </div>
+
     )
 }
 
--- frontend/src/pages/SignUp.jsx
+++ frontend/src/pages/SignUp.jsx
@@ -34,88 +34,86 @@ const SignUp = () => {
     };
 
     return (
-        <Layout>
 
-            <div className="container mt-5">
-                <div className="row justify-content-center">
-                    <div className="col-md-8">
-                        <img src="" alt="" />
-                    </div>
-                    <div className="col-md-4">
-                        <div className="card p-4">
+        <div className="container mt-5">
+            <div className="row justify-content-center">
+                <div className="col-md-8">
+                    <img src="" alt="" />
+                </div>
+                <div className="col-md-4">
+                    <div className="card p-4">
 
-                            <div className="card-body p-2">
-                                <div className="text-center">
-                                    <img className='logo' src={logo} alt="" />
-                                    <h3>Sign Up</h3>
-                                </div>
-                                <form onSubmit={handleSubmit}>
-                                    <div className="mb-3">
-                                        <label htmlFor="name" className="form-label">
-                                            Name
-                                        </label>
-                                        <input
-                                            type="text"
-                                            className="form-control"
-                                            id="name"
-                                            name="name"
-                                            value={name}
-                                            onChange={(e) => setName(e.target.value)}
-                                            required
-                                        />
-                                    </div>
-                                    <div className="mb-3">
-                                        <label htmlFor="email" className="form-label">
-                                            Email
-                                        </label>
-                                        <input
-                                            type="email"
-                                            className="form-control"
-                                            id="email"
-                                            name="email"
-                                            value={email}
-                                            onChange={(e) => setEmail(e.target.value)}
-                                            required
-                                        />
-                                    </div>
-                                    <div className="mb-3">
-                                        <label htmlFor="password" className="form-label">
-                                            Password
-                                        </label>
-                                        <input
-                                            type="password"
-                                            className="form-control"
-                                            id="password"
-                                            name="password"
-                                            value={password}
-                                            onChange={(e) => setPassword(e.target.value)}
-                                            required
-                                        />
-                                    </div>
-                                    <div className="mb-3">
-                                        <label htmlFor="phoneNumber" className="form-label">
-                                            Phone Number
-                                        </label>
-                                        <input
-                                            type="tel"
-                                            className="form-control"
-                                            id="phoneNumber"
-                                            name="phoneNumber"
-                                            value={phone}
-                                            onChange={(e) => setPhone(e.target.value)}
-                                            required
-                                        />
-                                    </div>
-                                    <button type="submit" className="btn submitBtn w-100">
-                                        Register
-                                    </button>
-                                </form>
+                        <div className="card-body p-2">
+                            <div className="text-center">
+                                <img className='logo' src={logo} alt="" />
+                                <h3>Sign Up</h3>
                             </div>
+                            <form onSubmit={handleSubmit}>
+                                <div className="mb-3">
+                                    <label htmlFor="name" className="form-label">
+                                        Name
+                                    </label>
+                                    <input
+                                        type="text"
+                                        className="form-control"
+                                        id="name"
+                                        name="name"
+                                        value={name}
+                                        onChange={(e) => setName(e.target.value)}
+                                        required
+                                    />
+                                </div>
+                                <div className="mb-3">
+                                    <label htmlFor="email" className="form-label">
+                                        Email
+                                    </label>
+                                    <input
+                                        type="email"
+                                        className="form-control"
+                                        id="email"
+                                        name="email"
+                                        value={email}
+                                        onChange={(e) => setEmail(e.target.value)}
+                                        required
+                                    />
+                                </div>
+                                <div className="mb-3">
+                                    <label htmlFor="password" className="form-label">
+                                        Password
+                                    </label>
+                                    <input
+                                        type="password"
+                                        className="form-control"
+                                        id="password"
+                                        name="password"
+                                        value={password}
+                                        onChange={(e) => setPassword(e.target.value)}
+                                        required
+                                    />
+                                </div>
+                                <div className="mb-3">
+                                    <label htmlFor="phoneNumber" className="form-label">
+                                        Phone Number
+                                    </label>
+                                    <input
+                                        type="tel"
+                                        className="form-control"
+                                        id="phoneNumber"
+                                        name="phoneNumber"
+                                        value={phone}
+                                        onChange={(e) => setPhone(e.target.value)}
+                                        required
+                                    />
+                                </div>
+                                <button type="submit" className="btn submitBtn w-100">
+                                    Register
+                                </button>
+                            </form>
                         </div>
                     </div>
                 </div>
             </div>
-        </Layout >
+        </div>
     )
 }
 
